cf5cc03fd297bb13b947fa9fdbff32589c5fa59e
Andrew Hexen <SyberHexen@gmail.com>
2023-08-10 10:30:55 -0700

    sm7325-common: wlan: update from DUBAI_RETAIL_13_T1RD33.116
    * add missing files and fix the wlan symlinks.
    * resolves the random high-latency/buffering issues when using 5ghz wifi
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I397104661ce80fa97c77b21be4188c5a8350a6aa

--- Android.mk
+++ Android.mk
@@ -109,9 +109,16 @@ $(RFS_MSM_WPSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 WIFI_FIRMWARE_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld
 $(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating wifi firmware symlinks: $@"
-	@mkdir -p $@
+	@mkdir -p $@/wlan
+	@mkdir -p $@/qca6490
+	@mkdir -p $@/qca6750
+	$(hide) ln -sf /mnt/vendor/persist/qca6490/wlan_mac.bin $@/qca6490/wlan_mac.bin
+	$(hide) ln -sf /vendor/etc/wifi/qca6490/WCNSS_mot_cfg.ini $@/qca6490/WCNSS_mot_cfg.ini
+	$(hide) ln -sf /vendor/etc/wifi/qca6490/WCNSS_qcom_cfg.ini $@/qca6490/WCNSS_qcom_cfg.ini
+	$(hide) ln -sf /mnt/vendor/persist/qca6750/wlan_mac.bin $@/qca6750/wlan_mac.bin
+	$(hide) ln -sf /vendor/etc/wifi/qca6750/WCNSS_mot_cfg.ini $@/qca6750/WCNSS_mot_cfg.ini
+	$(hide) ln -sf /vendor/etc/wifi/qca6750/WCNSS_qcom_cfg.ini $@/qca6750/WCNSS_qcom_cfg.ini
 	$(hide) ln -sf /vendor/etc/wifi/WCNSS_qcom_cfg.ini $@/WCNSS_qcom_cfg.ini
-	$(hide) ln -sf /mnt/vendor/persist/wlan_mac.bin $@/wlan_mac.bin
 
 ALL_DEFAULT_INSTALLED_MODULES += $(RFS_MSM_ADSP_SYMLINKS) $(RFS_MSM_CDSP_SYMLINKS) $(RFS_MSM_MPSS_SYMLINKS) \
                                  $(RFS_MSM_WPSS_SYMLINKS) $(RFS_MSM_SLPI_SYMLINKS) $(WIFI_FIRMWARE_SYMLINKS)
